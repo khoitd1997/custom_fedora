84692a900377007256411ae492c51ca9f1484152
khoitd1997 <khoidinhtrinh@gmail.com>
2019-04-06 16:06:33 -0700

    WIP: Added initial cfg files

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -x
 source settings.sh
 original_user=$(logname)
 currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cfg_file=${currDir}/${1}.cfg
 
 function cleanup {
 sudo -i -u ${original_user} bash << EOF
@@ -21,10 +22,12 @@ trap cleanup EXIT
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@" # request root access if not already
 sudo setenforce 0
 
+# sudo ln -sfv ${currDir}/${1}.cfg /etc/mock
+
 sudo -i -u ${original_user} bash << EOF
-mock -r fedora-"${env_fedora_ver}"-x86_64 --chroot "rm -rf /builddir/"
-mock -r fedora-"${env_fedora_ver}"-x86_64 --chroot "mkdir -p /builddir/"
-mock -r fedora-"${env_fedora_ver}"-x86_64 --copyin ${currDir}/build_fedora.sh ${currDir}/settings.sh ${currDir}/fedora-kickstarts /builddir
+mock -r ${cfg_file} --chroot "rm -rf /builddir/"
+mock -r ${cfg_file} --chroot "mkdir -p /builddir/"
+mock -r ${cfg_file} --copyin ${currDir}/build_fedora.sh ${currDir}/settings.sh ${currDir}/fedora-kickstarts /builddir
 mkdir -p ${currDir}/build
 mock -r fedora-${env_fedora_ver}-x86_64 --old-chroot --chroot /builddir/build_fedora.sh ${1}
 
