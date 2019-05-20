ce17f5e80d3b29219ae51f6f791559e00cb4a70e
khoitd1997 <khoidinhtrinh@gmail.com>
2019-05-19 23:12:46 -0700

    build(cli): Add cmake options

--- src/cli/build.sh
+++ src/cli/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${currDir}/build
+
+while (( "$#" )); do
+    case "$1" in
+        --clean)
+        rm -rf ./*
+        shift 1
+        ;;
+    esac
+done
 # rm -rf ${currDir}/build
 mkdir -p ${currDir}/build/bin/build/logs
 ln -sfv ~/hatter/example/example_settings.toml ~/hatter/src/cli/build/bin
@@ -8,21 +19,10 @@ ln -sfv ~/hatter/example/child_setting_1.toml ~/hatter/src/cli/build/bin
 mkdir -p ~/hatter/src/cli/build/bin/child_path
 ln -sfv ~/hatter/example/child_path/child_setting_2.toml ~/hatter/src/cli/build/bin/child_path
 
-cd ${currDir}/build
-
-while (( "$#" )); do
-    case "$1" in
-        --clean)
-        ninja clean
-        shift 1
-        ;;
-    esac
-done
-
-
 # cmake .. && make && ./bin/tomlparser ./bin/example_settings.toml
 # scan-build cmake -G Ninja .. && scan-build ninja && ./bin/tomlparser ./bin/settings.toml
 cmake -G Ninja .. && ninja 
+cmake -G Ninja -DUSE_CPPLINT=ON -DUSE_CPPCHECK=ON .. && ninja 
 cd bin
 ./tomlparser example_settings.toml
 # cmake -G Ninja .. && ninja && ./bin/tomlparser ./bin/random.toml
