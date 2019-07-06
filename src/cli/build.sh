4f60b7514e2ffe379014d797aa1038a977388d88
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-05 17:37:19 -0700

    WIP: Prepare for future gtest issue

--- src/cli/build.sh
+++ src/cli/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
+# set -x
 hatter_src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cmake_build_dir=/cmake_build_dir
 if [ -z "${IN_HATTER_DOCKER}" ] && [ -z "${CI}" ]; then
@@ -11,6 +12,7 @@ if [ -z "${IN_HATTER_DOCKER}" ] && [ -z "${CI}" ]; then
     # SYS_PTRACE because of sanitizer
     docker run --tty --rm -v cmake_build_vol:${cmake_build_dir} --name test_instance --cap-add SYS_PTRACE hattertest
 else
+    # rm -rf ${cmake_build_dir}/*
     if [ ! -z "${CI}" ]; then
         mkdir -p ${cmake_build_dir}
     fi
@@ -19,6 +21,9 @@ else
     dnf copr enable khoitd1997/toml11 -y
     
     test_build_root_dir=${cmake_build_dir}/bin
+    mkdir -p ${test_build_root_dir}
+    # cd ${test_build_root_dir}
+    # ls | rg -v hatter_cli | xargs rm -r
     mkdir -p ${test_build_root_dir}/build
     cat > ${test_build_root_dir}/build/env_var.sh << 'EOF'
     export env_os_name="example_setting"
@@ -45,17 +50,21 @@ EOF
     cp -r ${hatter_src_dir}/example/* ${env_user_supplied_dir}
     
     # export GTEST_FILTER="CommonSanitizeTest_*" # MUST SPECIFY HERE BEFORE THE TESTS ARE DISCOVERED
-    # GTEST_BREAK_ON_FAILURE=1 GTEST_COLOR=1 ctest --verbose --gtest_print_time=0
     # scan-build cmake -G Ninja .. && scan-build ninja && ./bin/hatter_cli ./bin/settings.toml
     cd ${cmake_build_dir}
-    cmake ${hatter_src_dir} && cmake --build .
+    cmake ${hatter_src_dir} -DRUN_TEST=ON && cmake --build .
+    
+    # run unit tests
+    # TODO(kd): Resolve gtest issue
+    # cd bin
+    # GTEST_BREAK_ON_FAILURE=1 GTEST_COLOR=1 ctest --verbose --gtest_print_time=0
     
     ./bin/hatter_cli
     if [ $? -ne 0 ]; then
         echo "toml parser failed"
     else
         echo "toml parser succeeded"
-        cp ${env_build_dir}/env_var.sh ${env_build_dir}/prev_env_var.sh
-        sed '/env_/s/env_/prev_env_/' -i ${env_build_dir}/prev_env_var.sh
+        cp ${env_build_dir}/env_var.sh ${env_prev_var_path}
+        sed '/env_/s/env_/prev_env_/' -i ${env_prev_var_path}
     fi
 fi
