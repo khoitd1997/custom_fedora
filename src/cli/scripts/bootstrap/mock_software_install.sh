a79e7b50e7b0abdf6a887d7eb8cb6d6de44b177c
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-20 15:46:54 -0700

    fix(cli): Removed unavailable func in mock sw install

--- src/cli/scripts/bootstrap/mock_software_install.sh
+++ src/cli/scripts/bootstrap/mock_software_install.sh
@@ -7,10 +7,8 @@ dnf install -y -q curl findutils git jq ripgrep \
                   squashfs-tools nano vim
 
 # install vscode repo
-print_info "installing vscode repo"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # install rpmfusion
-print_info "installing rpmfusion"
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y -q
