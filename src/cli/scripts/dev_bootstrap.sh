44c379287323fde29e52b21e111affc7e4a7103d
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-20 13:53:34 -0700

    refactor(cli): Refactored bootstrap

---
+++ src/cli/scripts/dev_bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# scripts used for installing dev tools inside docker
+set -e
+
+base_dnf_cmd="dnf install -y -q"
+# libs
+${base_dnf_cmd} spdlog libasan libubsan
+
+# build tools
+${base_dnf_cmd} ninja-build git cmake gcc-c++ clang
+
+# other tools
+${base_dnf_cmd} cppcheck gtest-devel gtest valgrind
+pip3 install cpplint -q
+
+# publishing tools
+${base_dnf_cmd} npm
+npm install semantic-release @semantic-release/github @semantic-release/commit-analyzer @semantic-release/release-notes-generator -D
---
+++ src/cli/scripts/mock_bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# launch at the initial creation of mock env
+
+set -e
+source /build_share/scripts/set_env_var.sh
+source ${env_script_dir}/misc_utils.sh
+
+# runtime tools
+dnf install -y -q curl findutils git jq ripgrep 
+                  fedora-workstation-repositories 
+                  dnf-plugins-core 
+
+print_info "cloning stock fedora-kickstart repo"
+git clone https://pagure.io/fedora-kickstarts.git ${env_stock_kickstart_dir}
+
+# install vscode repo
+print_info "installing vscode repo"
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# install rpmfusion
+print_info "installing rpmfusion"
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y -q
+
+print_info "refreshing dnf cache"
+dnf check-update -y -q
